6787ca3bfc7dc28bf1bc4d5d209abc65ccb4af5c
anaspoiala <ana.spoiala25@gmail.com>
2019-09-27 23:41:47 +0300

    Project setup

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string GameSceneName;
+    public string MainMenuSceneName;
+
+    public void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    public void OnMainMenuButtonPressed()
+    {
+        SceneManager.LoadScene(MainMenuSceneName);
+    }
+
+    public void OnExitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    private bool isPaused;
+    [SerializeField] private GameObject menuPanel;
+
+    private void Start()
+    {
+        isPaused = false;
+        Time.timeScale = 1;         // unpause
+        menuPanel.SetActive(false); // hide menu
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HandlePause();
+        }
+    }
+
+    private void HandlePause()
+    {
+        isPaused = !isPaused;           // toggle pause state
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;         // pause
+            menuPanel.SetActive(true);  // show menu
+        }
+        else
+        {
+            Time.timeScale = 1;         // unpause
+            menuPanel.SetActive(false); // hide menu
+        }
+    }
+}
