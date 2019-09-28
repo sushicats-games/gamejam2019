e851b16f27ac42f2ea461503a4ef2e02d9ccecbc
Eke Péter <peterekepeter@gmail.com>
2019-09-28 09:43:07 +0300

    Updated soil

---
+++ Assets/Scripts/Behaviour/Random3DRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random3DRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localRotation = Random.rotationUniform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
