a861f8f5220c9757b7149ca4cbd55d7f8fc9b47f
Eke Péter <peterekepeter@gmail.com>
2019-09-28 11:27:25 +0300

    Movement capability

---
+++ Assets/Scripts/Behaviour/MovementBasedOnMass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBasedOnMass : MonoBehaviour
+{
+    public float XMovespeedAtUnitMass;
+    public float YMovespeedAtUnitMass;
+    MovementCapability movementCapability;
+    new Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementCapability = GetComponent<MovementCapability>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementCapability.MovementX = XMovespeedAtUnitMass / rigidbody2D.mass;
+        movementCapability.MovementY = YMovespeedAtUnitMass / rigidbody2D.mass;
+    }
+}
---
+++ Assets/Scripts/State/MovementCapability.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCapability : MonoBehaviour
+{
+    public float MovementX = 1.0f;
+    public float MovementY = 1.0f;
+}
