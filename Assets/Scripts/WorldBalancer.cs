ef688bf2d5375b9ab809141665e5722c68655ffb
Eke Péter <peterekepeter@gmail.com>
2019-09-28 17:40:06 +0300

    World balancer makes sure that we never run out of gameplay

---
+++ Assets/Scripts/WorldBalancer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBalancer : MonoBehaviour
+{
+    float rebalanceTimer = 0.0f;
+    float initialTotalEnergy = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var energyState in UnityEngine.Object.FindObjectsOfType<EnergyState>())
+        {
+            initialTotalEnergy += energyState.Energy;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rebalanceTimer += Time.deltaTime;
+
+        if (rebalanceTimer > 60.0f)
+        {
+            rebalanceTimer -= 60.0f;
+            RebalanceLevel();
+        }
+
+    }
+
+    private void RebalanceLevel()
+    {
+        var currentTotalEnergy = 0.0f;
+        var gatherers = 0;
+        foreach (var energyState in UnityEngine.Object.FindObjectsOfType<EnergyState>())
+        {
+            if (energyState.GathersLostEnergy)
+            {
+                gatherers++;
+            }
+            currentTotalEnergy += energyState.Energy;
+        }
+
+        var shortage = initialTotalEnergy - currentTotalEnergy;
+        var resupplyGatherer = shortage / gatherers;
+
+        foreach (var energyState in UnityEngine.Object.FindObjectsOfType<EnergyState>())
+        {
+            if (energyState.GathersLostEnergy)
+            {
+                energyState.Energy += resupplyGatherer;
+            }
+        }
+
+    }
+}
