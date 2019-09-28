a4055d02e0ddfd6320431c97be286d60cff431ca
Eke Péter <peterekepeter@gmail.com>
2019-09-28 04:00:53 +0300

    Added algae meshes

---
+++ Assets/Scripts/Behaviour/ChooseOneAndSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseOneAndSpawn : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var chosen = Random.Range(0, gameObjects.Length);
+        Instantiate(gameObjects[chosen], this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
