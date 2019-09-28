49b263559a2832be7d00aee9384ff745735850eb
anaspoiala <ana.spoiala25@gmail.com>
2019-09-28 19:41:32 +0300

    Game scene + title

---
+++ Assets/Scripts/UI/FontEnlargeOnHover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FontEnlargeOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text text;
+
+    private int fontSize;
+
+    private void Start()
+    {
+        fontSize = text.fontSize;   
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.fontSize = fontSize * 2;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.fontSize = fontSize;
+    }
+}
